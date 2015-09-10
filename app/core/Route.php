166a9973a85c6453a60bdf3d64624c6a7457b274
Евгений Васильцов <eugenevasilsov@gmail.com>
2015-09-10 11:02:55 +0300

    fixed route.php (error404)

--- app/core/Route.php
+++ app/core/Route.php
@@ -78,9 +78,7 @@ class Route
 
     function ErrorPage404()
     {
-        $host = 'http://' . $_SERVER['HTTP_HOST'] . '/';
-        header('HTTP/1.1 404 Not Found');
-        header("Status: 404 Not Found");
-        header('Location:' . $host . '404');
+        $host = 'http://' . $_SERVER['HTTP_HOST'] . '/Error404';
+        header('Location:' . $host);
     }
 }
