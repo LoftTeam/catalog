e24c2badb55c249ad422aa4a83e059edc6507eca
Pavel Zakolodyazhnyi <grosesdorf@gmail.com>
2015-09-11 16:44:01 +0300

    link for SignIn

--- app/views/templates/_chunks/header.php
+++ app/views/templates/_chunks/header.php
@@ -3,15 +3,20 @@
         <a href="/">
             <img src="/images/logo/main.png" alt="">
         </a>
-        <span class="banner_seo_text">Магазин эоектронной техники</span>
+        <span class="banner_seo_text">Магазин электронной техники</span>
     </div>
     <div class="banner_title">
-        Магазин эоектронной техники
+        Магазин электронной техники
     </div>
     <div class="banner_phone">
-        <a class="banner_phone-link" href="tel:+84955466590">
-            <span>8(495)</span>
-            <span>546-65-90</span>
+<!--        <a class="banner_phone-link" href="tel:+80481112233">-->
+            <span>8(048)</span>
+            <span>111-22-33</span>
+        </a>
+        </div>
+        <div class="authentication">
+        <a class="auth-link" href="auth">
+            <span>Войти</span>
         </a>
     </div>
 </header>
