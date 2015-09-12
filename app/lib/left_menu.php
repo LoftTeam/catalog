7a4d52dddbdbba8c6b7fd549a1282eedfc7ceb0f
Евгений Васильцов <eugenevasilsov@gmail.com>
2015-09-12 18:58:45 +0300

    add left_menu products

---
+++ app/lib/left_menu.php
@@ -0,0 +1,32 @@
+
+<?php
+
+class Lib extends  Model
+{
+    function get_left_menu()
+    {
+        $sql = "SELECT id, title FROM category_products";
+
+        $result = $this->_pdo->query($sql);
+
+        if(!$result){
+            return $result;
+        }
+        $records = $result->fetchAll(PDO::FETCH_ASSOC);
+        return $records;
+    }
+
+    function get_left_menu_items_by_category_id($id)
+    {
+        $sql = "SELECT id , title FROM products WHERE id_catalog = {$id}";
+
+        $result = $this->_pdo->query($sql);
+
+        if(!$result){
+            return $result;
+        }
+        $records = $result->fetchAll(PDO::FETCH_ASSOC);
+        return $records;
+    }
+}
+
